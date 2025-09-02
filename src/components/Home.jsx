af84b60e320da9814fc3508ccb69a925f2d64b69
UnicornDad8 <cecibenitezca@gmail.com>
2025-09-02 11:21:55 -0300

    Finish Hero section

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,14 +9,29 @@ import teamLogo6 from "../assets/team-logo-6.svg";
 
 const Home = () => {
   return (
-    <div className="flex flex-col h-[calc(100vh - 295px)] gap-[20px]">
+    <div className="flex flex-col h-[650px] gap-[20px] justify-between">
       <div className="grid lg:grid-cols-2 grid-cols-1 gap-[20px]">
-        <div className="h-[550px] bg-blue-500 px-5 md:px-3">1</div>
-        <div className="h-[550px] px-5 md:px-3 flex items-center justify-center">
+        <div className="px-5 md:px-3 flex flex-col justify-center">
+          <h3 className="text-md lg:text-lg text-dark-text-500 font-semibold mb-6">🔥 Solid is one of the best options for you</h3>
+          <h1 className="font-black text-3xl lg:text-4xl text-dark-text-500">
+          Complete Tailwind CSS <br /><mark className="bg-highlight-500">Template</mark> for SaaS Website
+          </h1>
+          <p className="text-gray-300 text-sm lg:text-md my-6">
+          Solid is a complete Tailwind CSS template crafted specially for SaaS, Software <br /> Mobile App and Web App Sites.
+          </p>
+          <div className="flex space-x-6">
+            <input type="text" placeholder="Enter your email address" className="border-2 border-gray-200 py-3 px-6 shadow-lg shadow-gray-100 rounded-4xl placeholder:text-gray-400 placeholder:font-medium focus:outline-none focus:border-2 focus:border-primary-500" />
+            <button className="text-white font-medium py-3 px-6 bg-dark-text-500 rounded-4xl hover:bg-black transition-all cursor-pointer">Get Started</button>
+          </div>
+          <p className="text-dark-text-500 text-sm lg:text-md py-6">
+          Try for free no credit card required
+          </p>
+        </div>
+        <div className="px-5 md:px-3 flex items-center justify-center">
           <img src={heroImg} width={700} height={444} className="shadow-2xl shadow-gray-300" />
         </div>
       </div>
-      <div className="bg-light-200 w-full h-[90px] grid grid-cols-3 lg:grid-cols-6">
+      <div className="bg-light-200 w-full h-[90px] grid grid-cols-3 lg:grid-cols-6 place-items-end">
         <div className="flex items-center justify-center w-full p-3">
           <img src={teamLogo1} width={100} height={30} className="brightness-125" />
         </div>
